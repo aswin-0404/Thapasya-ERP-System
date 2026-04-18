fda0deb20e0a7b324f096aae320c0cea61cce41d
aswin-0404 <aswinofficial101@gmail.com>
2026-04-18 20:25:10 +0530

    fix router imports explicitly

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import student, staff, enquiry, course, booking, auth, role, branch
+from app.api.v1.endpoints import enquiry, course, booking
+
+from app.api.v1.endpoints.student import router as student_router
+from app.api.v1.endpoints.staff import router as staff_router
+from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.role import router as role_router
+from app.api.v1.endpoints.branch import router as branch_router
 
 api_router = APIRouter()
 
 # Aswin Routes
 
-api_router.include_router(student.router, prefix="/student", tags=["Students"])
-api_router.include_router(staff.router, prefix="/staff", tags=["Staff"])
-api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(role.router)
-api_router.include_router(branch.router)
+api_router.include_router(student_router, prefix="/student", tags=["Students"])
+api_router.include_router(staff_router, prefix="/staff", tags=["Staff"])
+api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+api_router.include_router(role_router)   
+api_router.include_router(branch_router)
 
 
 # Prince Routes
